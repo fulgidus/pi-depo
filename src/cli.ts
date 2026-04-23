30307fd9b3aaa6d9aa84a5b5034ff76cc925602c
Alessio Corsi <alessio.corsi@gmail.com>
2026-04-24 00:19:07 +0200

    chore: bump to 0.1.2

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ import { loadManifest } from "./sync.js";
 import { readFile } from "node:fs/promises";
 import { kitYmlPath } from "./config.js";
 
-const VERSION = "0.1.1";
+const VERSION = "0.1.2";
 
 const main = defineCommand({
   meta: {
